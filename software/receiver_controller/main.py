2199afd8568772375cbe392def44f788ee9d9bb8
bjohan <johan.o.bodin@gmail.com>
2020-08-24 22:41:17 +0200

    Testing filters and gain in low band receiver

--- software/receiver_controller/main.py
+++ software/receiver_controller/main.py
@@ -22,20 +22,56 @@ port = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 print("waiting for arduino to sleep");
 time.sleep(1) #allow time to boot
 
+
+
 print("creating client");
 c = corbomiteClient.CorbomiteClient(port)
 autocalEnable = c.widgets['AUTOCAL_ENABLE']
 calTrig = c.widgets['CAL_TRIG']
+
 lru1 = c.widgets['LRU1_LOW']
 lru2 = c.widgets['LRU2_LOW']
-g = c.widgets['G_LOW'];
-lru1.writeValue(True)
-lru2.writeValue(True)
-g.writeValue(True)
-print("Enabling BITE");
+bl_low = c.widgets['BL_LOW']
+g = c.widgets['G_LOW']
 
-autocalEnable.writeValue(True)
-calTrig.writeValue(True)
+bl_low.writeValue(False)
+
+def setFilter0():
+    #600MHz to 1300MHz
+    print "Enabling filter from 600MHz to 1300MHz"
+    lru1.writeValue(False)
+    lru2.writeValue(False)
+
+
+def setFilter1():
+    #1030MHz to 2300MHz
+    print "Enabling filter from 1030MHz to 2300MHz"
+    lru1.writeValue(True)
+    lru2.writeValue(False)
+
+def setFilter2():
+    #1800MHz to 3900MHz
+    print "Enabling filter from 1800Mhz to 3900Mhz"
+    lru1.writeValue(False)
+    lru2.writeValue(True)
+
+def setFilter3():
+    #3050MHz to 6000MHz
+    print "Enabling filter from 3050MHz to 6000MHz"
+    lru1.writeValue(True)
+    lru2.writeValue(True)
+
+def enableBite():
+    print("Enabling BITE");
+    autocalEnable.writeValue(True)
+    calTrig.writeValue(True)
+
+
+setFilter1()
+enableBite()
+
+#quit()
+g.writeValue(True)
 
 print("Setting up LMX");
 time.sleep(1)
@@ -44,7 +80,7 @@ sys.path.append('../../external_dependencies/Lmx2594/py/')
 import lmx2594
 w = c.widgets
 hwif = corbomite_lmx_interface.CorbomiteLmxInterface(w['registerAddress'], w['writeReg'], w['readReg'], w['read'],  w['write'], w['CE_LO_HI']);
-lmx = lmx2594.Lmx2594(hwif, fosc=320e6)
+lmx = lmx2594.Lmx2594(hwif, fosc=320.069334e6)
 lmx.reset()
 lmx.applyConfig('../../external_dependencies/Lmx2594/py/10GOut320MRef.txt')
 #print lmx.getFpd()/1e6
@@ -61,7 +97,7 @@ lmx.applyConfig('../../external_dependencies/Lmx2594/py/10GOut320MRef.txt')
 #for i in np.linspace(600e6, 2600e6, 200):
 #    lmx.setFrequency(i)
 #    time.sleep(3)
-lmx.setFrequency(619e6)
+lmx.setFrequency(1100e6)
 #after = getAllFields(lmx)
 #compareFields(before, after)
 #for fn in lmx.getAllFieldNames():
@@ -69,6 +105,15 @@ lmx.setFrequency(619e6)
 #    print("%s:"%(fn), v)
 print("done")
 print("Lock status:", lmx.isLocked())
+while True:
+    g.writeValue(True)
+    time.sleep(1)
+    g.writeValue(False)
+    time.sleep(1)
+time.sleep(10000)
+for i in np.arange(300e6, 2000e6, 1e6):
+    lmx.setFrequency(i)
+    print "Locked", lmx.isLocked(), "frequency", i/1e6
 #hwif.chipEnable(False)
 #for fn in lmx.getAllFieldNames():
 #    v = lmx.getField(fn)
